210c13982092e68c37fdc62644b77946e83ba35f
Gabriel Araujo <gabriel.houth@gmail.com>
2022-07-23 11:45:17 -0300

    feat: add refresh token generation

--- src/modules/auth/application/usecases/sign-in.ts
+++ src/modules/auth/application/usecases/sign-in.ts
@@ -1,8 +1,8 @@
 import jwt from 'jsonwebtoken';
 import bcryptjs from 'bcryptjs';
 
-import { IAuthRepository } from '../repositories/sign-in';
 import { BaseError } from '../../../../core/errors/base-error';
+import { IAuthRepository, UserDTO } from '../repositories/sign-in';
 import { Either, left, right } from '../../../../app/helpers/either';
 import { InvalidCredentialsError } from '../../adapters/errors/invalid-credentials';
 
@@ -35,13 +35,12 @@ class SignInUsecaseImpl implements ISignInUsecase {
     const userOrError = await this.authRepository.signIn(email, password);
 
     if (userOrError.isLeft()) {
-      return left(userOrError.value);
+      const error: BaseError = userOrError.value;
+      return left(error);
     }
 
-    const isUserAuth: boolean = bcryptjs.compareSync(
-      password,
-      userOrError.value.password,
-    );
+    const user: UserDTO = userOrError.value;
+    const isUserAuth: boolean = bcryptjs.compareSync(password, user.password);
 
     if (!isUserAuth) {
       return left(
@@ -52,19 +51,24 @@ class SignInUsecaseImpl implements ISignInUsecase {
     }
 
     const accessToken: string = jwt.sign(
-      userOrError.value.uid,
+      user.uid,
       process.env.SECRET_ACCESS_TOKEN ?? 'ced7cafb-ca79-4b39-b154-b99afaed33b7',
-      {
-        expiresIn: process.env.ACCESS_TOKEN_EXPIRATION ?? '15m',
-      },
+      { expiresIn: process.env.ACCESS_TOKEN_EXPIRATION ?? '15m' },
+    );
+
+    const refreshToken: string = jwt.sign(
+      user.uid,
+      process.env.SECRET_REFRESH_TOKEN ??
+        '78c24895-5fe3-4eb6-8266-6f2726be4f7d',
+      { expiresIn: process.env.REFRESH_TOKEN_EXPIRATION ?? '30d' },
     );
 
     const userCredentials: UserCredentialsDTO = {
-      uid: userOrError.value.uid,
-      name: userOrError.value.name,
-      email: userOrError.value.email,
+      uid: user.uid,
+      name: user.name,
+      email: user.email,
       accessToken: accessToken,
-      refreshToken: '',
+      refreshToken: refreshToken,
     };
 
     return right(userCredentials);
