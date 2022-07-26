eef870a0a5bec84ba2f67d14c97a4290f1448977
Gabriel Araujo <gabriel.houth@gmail.com>
2022-07-26 14:50:12 -0300

    test: add a test to email validation

---
+++ src/app/utils/email-validation.spec.ts
@@ -0,0 +1,15 @@
+import { isEmailValid } from './email-validation';
+
+describe('', () => {
+  it('should return true if email is a valid email.', () => {
+    const fakeEmail = 'gabriel.houth@gmail.com';
+
+    expect(isEmailValid(fakeEmail)).toBeTruthy();
+  });
+
+  it('should return false if email is not a valid email.', () => {
+    const fakeEmail = 'gabr1el.h@uth@@gmail.com';
+
+    expect(isEmailValid(fakeEmail)).toBeFalsy();
+  });
+});
